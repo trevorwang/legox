691d1460df38e5e3a6e2194c27325725937ded0f
Trevor Wang <trevor.wang@qq.com>
2020-12-14 20:30:41 +0800

    base gradle

--- buildSrc/src/main/java/legox/plugin/VersionPlugin.kt
+++
@@ -1,9 +0,0 @@
-package legox.plugin
-
-//import org.gradle.api.Plugin
-//import org.gradle.api.Project
-//
-//class VersionPlugin : Plugin<Project> {
-//    override fun apply(project: Project) {
-//    }
-//}
