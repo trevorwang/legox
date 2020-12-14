43c3f208a88c61bf54b1028930b595317f401c5f
Trevor Wang <trevor.wang@qq.com>
2020-12-14 20:20:20 +0800

    build src

---
+++ buildSrc/src/main/java/legox/plugin/BuildConfig.kt
@@ -0,0 +1,10 @@
+package legox.plugin
+
+object BuildConfig {
+    val compileSdkVersion = 30
+    val buildToolsVersion = "30.0.2"
+    val minSdkVersion = 21
+    val targetSdkVersion = 30
+    val versionCode = 10000
+    val versionName = "1.0.0"
+}
---
+++ buildSrc/src/main/java/legox/plugin/VersionPlugin.kt
@@ -0,0 +1,9 @@
+package legox.plugin
+
+//import org.gradle.api.Plugin
+//import org.gradle.api.Project
+//
+//class VersionPlugin : Plugin<Project> {
+//    override fun apply(project: Project) {
+//    }
+//}
